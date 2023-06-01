da6f5cf2b14f6adea2a75f9ea6702b4ca3a3dfd6
Alexandre Retamero <alexandreretamero@gmail.com>
2023-06-01 16:23:58 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
 import MyRoutes from './routes';
 
+// Toastify
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 function App() {
   return (
     <div className="app">
+      <ToastContainer autoClose={3000} theme="dark" />
       <MyRoutes />
     </div>
   );

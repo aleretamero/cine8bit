0ba2ab85b90b0830db13e85021a9fc83ab94396b
Alexandre Retamero <alexandreretamero@gmail.com>
2023-06-02 13:17:10 -0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
-import './assets/sass/reset.scss';
-import './assets/sass/globals.scss';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>

463fbbc5289eae7f7f06b3694c0b1f3ef1971ee9
Alexandre Retamero <alexandreretamero@gmail.com>
2023-06-02 11:29:17 -0300

    Update movie.ts

--- src/types/movie.ts
+++ src/types/movie.ts
@@ -1,8 +1,8 @@
 export default interface iMovie {
   id:number
   title:string
-  backdrop_path:string
-  poster_path:string
-  overview:string
-  vote_average:number
+  backdrop_path?:string
+  poster_path?:string
+  overview?:string
+  vote_average?:number
 }

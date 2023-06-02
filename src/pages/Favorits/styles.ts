8b3b04da7a708ec0a2494b6fb538dfeff23b9ef0
Alexandre Retamero <alexandreretamero@gmail.com>
2023-06-02 11:29:28 -0300

    Create styles.ts

---
+++ src/pages/Favorits/styles.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const Container = styled.section`
+  margin-top: 2rem;
+  display: flex;
+  flex-direction: column;
+`;
+
+export const Movie = styled.li`
+  display: flex;
+`;

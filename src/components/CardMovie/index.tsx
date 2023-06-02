074a6f59d42e4d97096c255ed7dd0903b6cb3fc7
Alexandre Retamero <alexandreretamero@gmail.com>
2023-06-02 11:28:49 -0300

    Create index.tsx

---
+++ src/components/CardMovie/index.tsx
@@ -0,0 +1,19 @@
+import * as Styled from './styles';
+
+import iMovie from '../../types/movie';
+
+const CardMovie = ({ id, title, poster_path }: iMovie) => {
+  return (
+    <Styled.CardMovie>
+      <Styled.LinkMovie to={`/filme/${id}`}>
+        <Styled.TitleMovie>{title}</Styled.TitleMovie>
+        <Styled.ImgMovie
+          src={`https://image.tmdb.org/t/p/original/${poster_path}`}
+          alt={title}
+        />
+      </Styled.LinkMovie>
+    </Styled.CardMovie>
+  );
+};
+
+export default CardMovie;

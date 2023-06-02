c72ad9a3a746ed696656763b304ece66e9e4e757
Alexandre Retamero <alexandreretamero@gmail.com>
2023-06-02 13:17:20 -0300

    Create Theme.ts

---
+++ src/global/styles/Theme.ts
@@ -0,0 +1,15 @@
+export const darkTheme = {
+  title: 'dark',
+  colors: {
+    background: '#121212',
+    text: '#fff',
+  },
+};
+
+export const lightTheme = {
+  title: 'light',
+  colors: {
+    background: '#fff',
+    text: '#121212',
+  },
+};

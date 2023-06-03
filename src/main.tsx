e5fd8f8fbda347944e74630de792f024fda30e61
Alexandre Retamero <alexandreretamero@gmail.com>
2023-06-03 16:34:02 -0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 
+import { GlobalProvider } from './contexts/GlobalContext.tsx';
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <GlobalProvider>
+      <App />
+    </GlobalProvider>
   </React.StrictMode>
 );

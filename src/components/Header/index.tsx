84b35c28831dfde1b320876f03d2c578df624357
Alexandre Retamero <alexandreretamero@gmail.com>
2023-06-03 16:33:46 -0300

    Update index.tsx

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,16 @@
 import * as Styled from './styles';
 import { Link } from 'react-router-dom';
 import Switch from 'react-switch';
+import { LightModeIcon, DarkModeIcon } from '../../assets/themeIcons';
+
+import { useThemeContext } from '../../hooks/useThemeContext';
+import { TypeThemeContext } from '../../contexts/GlobalContext';
+
 import Logo from '../Logo';
 
 const Header = () => {
+  const { themeStorage, toggleTheme } = useThemeContext() as TypeThemeContext;
+
   return (
     <Styled.Header>
       <Link to="/">
@@ -11,17 +18,16 @@ const Header = () => {
       </Link>
       <Styled.Navbar>
         <Link to="/favoritos">Favoritos</Link>
-        <Switch
-          onChange={() => {}}
-          checked={true}
-          checkedIcon={false}
-          uncheckedIcon={false}
-          height={19}
-          width={40}
-          handleDiameter={20}
-          offColor="#00f"
-          onColor="#f00"
-        />
+        {toggleTheme && (
+          <Switch
+            onColor="#00008B"
+            offColor="#62ABD9"
+            checkedIcon={DarkModeIcon}
+            uncheckedIcon={LightModeIcon}
+            onChange={toggleTheme}
+            checked={themeStorage}
+          />
+        )}
       </Styled.Navbar>
     </Styled.Header>
   );

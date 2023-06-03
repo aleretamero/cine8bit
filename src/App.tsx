c25cd3fee0efb9bdd99334ce99f4b69bb6823f5b
Alexandre Retamero <alexandreretamero@gmail.com>
2023-06-03 16:33:41 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,25 @@
 import { GlobalStyle } from './global/styles/Global';
 
+import MyRoutes from './routes';
+
+import { useThemeContext } from './hooks/useThemeContext';
+import { TypeThemeContext } from './contexts/GlobalContext';
 import { ThemeProvider } from 'styled-components';
 import { darkTheme, lightTheme } from './global/styles/Theme';
 
-import MyRoutes from './routes';
-
-// Toastify
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
+  const { themeStorage } = useThemeContext() as TypeThemeContext;
+
   return (
-    <ThemeProvider theme={lightTheme}>
+    <ThemeProvider theme={themeStorage ? darkTheme : lightTheme}>
       <GlobalStyle />
-      <ToastContainer autoClose={3000} theme="dark" />
+      <ToastContainer
+        autoClose={3000}
+        theme={themeStorage ? 'light' : 'dark'}
+      />
       <MyRoutes />
     </ThemeProvider>
   );

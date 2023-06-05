e6673fd3f4197e47a119eb0c9858e6ec4d261397
Alexandre Retamero <alexandreretamero@gmail.com>
2023-06-05 14:23:19 -0300

    Update styles.ts

--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -8,18 +8,30 @@ export const Container = styled.section`
   align-items: center;
   gap: 2rem;
   padding: 2rem;
+
+  @media (max-width: 425px) {
+    padding: 1rem;
+  }
 `;
 
 export const Title = styled.h1`
   font-size: 3rem;
+
+  @media (max-width: 475px) {
+  font-size: 2rem;
+  }
 `;
 
 export const ListMovies = styled.ul`
   max-width: 1200px;
   width: 100%;
   display: grid;
-  grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+  grid-template-columns: repeat(auto-fill, minmax(220px, 1fr));
   gap: 2rem;
+
+  @media (max-width: 425px) {
+    gap: 1rem;
+  }
 `;
 
 export const Loading = styled.p`

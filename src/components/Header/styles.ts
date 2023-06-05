f7405d10f51abfd10824f071bf7b4e2c04c0e816
Alexandre Retamero <alexandreretamero@gmail.com>
2023-06-05 14:23:05 -0300

    Update styles.ts

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -2,17 +2,25 @@ import styled from 'styled-components';
 
 export const Header = styled.header`
   width: 100%;
-  height: 80px;
-  padding: 0 max(calc((100% - 1200px) / 2), 2rem);
+  /* height: 80px; */
+  padding: 1rem max(calc((100% - 1200px) / 2), 2rem);
   display: flex;
+  flex-wrap: wrap;
+  gap: 1rem;
   justify-content: space-between;
   align-items: center;
   background-color: #000;
+
+  @media (max-width: 450px) {
+    justify-content: center;
+  }
 `;
 
 export const Navbar = styled.nav`
   display: flex;
-  font-size: 1.5rem;
+  align-items: center;
   gap: 1rem;
+  /* font-size: 1.5rem; */
+  /* gap: 1rem; */
   color: #fff;
 `;
